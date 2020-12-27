257c4d07e76d4676522e2c04e724e5aa172be32e
范平凡 <1742020289@qq.com>
2020-12-27 16:29:55 +0800

    createComponents

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,29 @@
+<template>
+   <div id="main">
+    <Header title="登录喵喵"></Header>
+    <div id="content">
+      <Login></Login>
+    </div>
+    <TabBar></TabBar>
+  </div>
+</template>
+
+<script>
+
+import Login from '@/components/Login'
+import Header from '@/components/Header';
+import TabBar from '@/components/TabBar';
+
+export default {
+    name :'Mine',
+    components:{
+        Header,
+        TabBar,
+        Login
+    }
+}
+</script>
+
+<style>
+
+</style>
