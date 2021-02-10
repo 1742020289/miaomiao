dd771b14130b572bfd6e589a63e075704d584158
范平凡 <1742020289@qq.com>
2021-02-10 17:52:37 +0800

    setData

--- src/main.js
+++ src/main.js
@@ -2,8 +2,32 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+Vue.prototype.axios=axios;
+Vue.filter('actorsfilter', (data) => {
+  if (!data) {
+    return '暂无主演'
+  }
+  var result = data.map(item => item.name)
+  return result.join(' ')
+});
+Vue.filter('filter',  (path)=> {
+  return path.replace('w.h', '64.90')
+});
+Vue.filter('gradeFilter',(data)=>{
+  if(data===0){
+    return '暂无评分';
+  }
+  return data;
+})
+Vue.filter('price',(data)=>{
+  return data/100;
+});
+Vue.filter('distance',(data)=>{
+  return Math.floor(data*100)/100;
+})
 
 new Vue({
   router,
